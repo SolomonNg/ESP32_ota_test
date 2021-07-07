a5e286afdc28fcf30235cadf8f4947ad2b3c3e60
SolomonNg <yuklun20110719@gmail.com>
2021-07-07 13:07:13 +0800

    add led blink

--- app/start.py
+++ app/start.py
@@ -1,1 +1,12 @@
-print('Version 2 installed using USB') 
+from machine import Pin
+import time
+
+
+print('Version 2 installed using USB')
+
+p0 = Pin(2, Pin.OUT)    # create output pin on GPIO0
+while True:
+    p0.on()                 # set pin to "on" (high) level
+    time.sleep(1)
+    p0.off()                # set pin to "off" (low) level
+    time.sleep(1)
